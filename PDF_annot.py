a04a60cf882af2deb48c7dae391c026c85453db8
KhalilAMARDJIA <khalil.amardjia@gmail.com>
2023-03-17 19:33:22 +0100

    factored PDF_annot

--- PDF_annot.py
+++ PDF_annot.py
@@ -1,48 +1,76 @@
 import fitz
 import spacy
+import os
 
+def highlight_pdf(pdf_file_path: str, output_file_path: str, model: str, colors: dict):
+    """
+    Highlights named entities in a PDF file using the SpaCy NER model and saves the modified PDF file.
 
-# Force SpaCy to use GPU
-spacy.prefer_gpu()
+    Args:
+        pdf_file_path (str): The path to the input PDF file.
+        output_file_path (str): The path to the output PDF file.
+        model (str): The name of the SpaCy model to use.
+        colors (dict): A dictionary that maps entity types to colors.
 
-# Load the SpaCy model
-nlp = spacy.load("Model/model-best")
+    Returns:
+        None
+    """
+    # Load the SpaCy model
+    nlp = spacy.load(model)
 
-# Define colors for each entity type
-colors = {"OUTCOME": [0.8, 1, 0.8],  # light green color (r, g, b)
-          "ADVERSE_EVENT": [1, 0.8, 0.8],  # light red color (r, g, b)
-          "OTHER": [1, 1, 0.8],  # light yellow color (r, g, b)
-          "DEVICE": [0.8, 0.8, 1]}  # light blue color (r, g, b)
+    # Open the PDF file using PyMuPDF
+    pdf_file = fitz.open(pdf_file_path)
 
-# Open the PDF file using PyMuPDF
-pdf_file = fitz.open("pdfs/test2.pdf")
+    # Loop over each page in the PDF file
+    for page_num in range(len(pdf_file)):
+        # Load the current page
+        page = pdf_file.load_page(page_num)
+        # Perform NER on the page's text
+        text = page.get_text()
+        doc = nlp(text)
 
-# Loop over each page in the PDF file
-for page_num in range(len(pdf_file)):
-    # Load the current page
-    page = pdf_file.load_page(page_num)
-    # Perform NER on the page's text
-    text = page.get_text()
-    doc = nlp(text)
+        # Loop over each named entity and find its location on the page
+        for entity in doc.ents:
+            # Get the coordinates of the named entity
+            try:
+                bbox = page.search_for(entity.text)[0]  # find the first occurrence
+            except:
+                pass
 
-    # Loop over each named entity and find its location on the page
-    for entity in doc.ents:
-        # Get the coordinates of the named entity
-        try:
-            bbox = page.search_for(entity.text)[0]  # find the first occurrence
-        except:
-            pass
+            # Get the color for this entity type
+            if entity.label_ in colors:
+                color = colors[entity.label_]
+            else:
+                color = colors["OTHER"]
 
-        # Get the color for this entity type
-        if entity.label_ in colors:
-            color = colors[entity.label_]
-        else:
-            color = colors["OTHER"]
+            # Add a highlight annotation to the PDF page with the appropriate color
+            highlight = page.add_highlight_annot(bbox)
+            highlight.set_colors(stroke=color)
+            highlight.update()
 
-        # Add a highlight annotation to the PDF page with the appropriate color
-        highlight = page.add_highlight_annot(bbox)
-        highlight.set_colors(stroke=color)
-        highlight.update()
+    # Save the modified PDF file
+    pdf_file.save(output_file_path)
 
-# Save the modified PDF file
-pdf_file.save("pdfs/test_highlighted.pdf")
+if __name__ == "__main__":
+# Define the colors dictionary
+    colors = {
+        "OUTCOME": [0.8, 1, 0.8],         # light green color (r, g, b)
+        "ADVERSE_EVENT": [1, 0.8, 0.8],   # light red color (r, g, b)
+        "OTHER": [1, 1, 0.8],             # light yellow color (r, g, b)
+        "DEVICE": [0.8, 0.8, 1]           # light blue color (r, g, b)
+    }
+
+    # Define the SpaCy model to use
+    model = "Model/model-best"
+
+    # Get the list of PDF files in the pdfs folder
+    pdf_files = [f for f in os.listdir("pdfs") if f.endswith(".pdf")]
+
+    # Loop over each PDF file
+    for pdf_file in pdf_files:
+        # Construct the input and output file paths
+        input_file_path = os.path.join("pdfs", pdf_file)
+        output_file_path = os.path.join("pdfs", "output", f"{os.path.splitext(pdf_file)[0]}_NER.pdf")
+
+        # Highlight the named entities in the PDF file
+        highlight_pdf(input_file_path, output_file_path, model, colors)
