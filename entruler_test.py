a970c077ab3b9d5e2f1c5dbeb40378c6b6e6f4bf
KhalilAMARDJIA <khalil.amardjia@gmail.com>
2023-03-30 15:01:15 +0200

    Entityruler on top of NER

---
+++ entruler_test.py
@@ -0,0 +1,20 @@
+import spacy
+
+
+# try adding entityruler on top of ner
+
+nlp = spacy.load('Model/scibert_scivocab_uncased')
+
+
+# add entity ruler after ner
+ruler = nlp.add_pipe("entity_ruler")
+ruler.from_disk("input/matcher_db/entity_ruler_df.jsonl")
+
+
+text = "the patient suffered a cystoid macular edema (CME) and Glistening and suprachoroidal hemorrhage after 3 months follow-up."
+
+doc = nlp(text)
+
+
+for ent in doc.ents:
+    print(ent.text, ent.label_)
