092fd18dabfb484cbb36b3c4f9bbb9e43ab7dd18
KhalilAMARDJIA <khalil.amardjia@gmail.com>
2023-03-30 15:28:32 +0200

    Entityruler on top of NER

--- entruler_test.py
+++ entruler_test.py
@@ -10,11 +10,14 @@ nlp = spacy.load('Model/scibert_scivocab_uncased')
 ruler = nlp.add_pipe("entity_ruler")
 ruler.from_disk("input/matcher_db/entity_ruler_df.jsonl")
 
+nlp.disable_pipe("ner")
+nlp.pipe_names
 
-text = "the patient suffered a cystoid macular edema (CME) and Glistening. The suprachoroidal hemorrhage score after 3 months follow-up."
+text = "the patient suffered a cystoid macular edema (CME) and Glistening. The suprachoroidal hemorrhage score after 3 months follow-up was Accommodative amplitude and contrast sensitivity and pupil size."
+
+
 
 doc = nlp(text)
-
 
 for ent in doc.ents:
     print(ent.text, ent.label_)
