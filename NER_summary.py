ca5643d4cc5d43cfec376df9a71bfca4753151f1
KhalilAMARDJIA <khalil.amardjia@gmail.com>
2023-04-03 12:24:24 +0200

    additional training data

--- NER_summary.py
+++ NER_summary.py
@@ -1,33 +1,52 @@
 import spacy
 import pandas as pd
 
-# load spacy model
-nlp = spacy.load("Model/scibert_scivocab_cased")
+def extract_ner_from_data(input_file, output_file, model_name, id_col='pubmed_id', abstract_col='abstract', separator=';'):
+    # load spacy model
+    nlp = spacy.load(model_name)
 
-nlp.prefer_gpu()
+    # load data
+    data = pd.read_csv(input_file, sep=separator)
 
-data = pd.read_csv('input/raw_data.csv', sep=";")
+    # remove rows where abstract is null
+    data = data[data[abstract_col].notna()]
 
-data = data[data['abstract'].notna()].sample(100)
+    # create a empty columns for each nlp ner label name in the data
+    for column in nlp.pipe_labels['ner']:
+        data[column] = ""
 
-zip_data = zip(data['abstract'], data['pubmed_id'])
+    # for each pmid matching pubmed_id, store doc.ents in its corresponding column
+    # when multiple entities are found, separate them by a tab
+    zip_data = zip(data[abstract_col], data[id_col])
+    for doc, pmid in nlp.pipe(zip_data, as_tuples=True):
+        for ent in doc.ents:
+            if ent.label_ in data.columns:
+                if data.loc[data[id_col] == pmid, ent.label_].empty:
+                    data.loc[data[id_col] == pmid, ent.label_] = ent.text
+                else:
+                    data.loc[data[id_col] == pmid, ent.label_] += ent.text + '\t'
+
+    # remove duplicate row wise and keep the longest string
+    for column in nlp.pipe_labels['ner']:
+        data[column] = data[column].apply(lambda x: remove_duplicate_keep_longest(x))
+
+    # save data to csv file
+    data.to_csv(output_file, sep=separator, index=False)
+    
+def remove_duplicate_keep_longest(text):
+    """
+    Remove duplicate and keep the longest string from a tab-separated string
+    """
+    if isinstance(text, str):
+        entities = text.split('\t')
+        entities = sorted(entities, key=len, reverse=True) # sort by length in descending order
+        dedup_entities = []
+        for entity in entities:
+            if not any(entity in e for e in dedup_entities):
+                dedup_entities.append(entity)
+        return '\t'.join(dedup_entities)
+    else:
+        return text
 
 
-# create a empty columns for each nlp ner label name in the data
-
-for column in  nlp.pipe_labels['ner']:
-    data[column] = ""
-
-
-
-# for each pmid matching pubmed_id, store doc.ents in its corresponding column
-# when multiple entities are found, separate them by a tab
-for doc, pmid in nlp.pipe(zip_data, as_tuples=True):
-    for ent in doc.ents:
-        if ent.label_ in data.columns:
-            if data.loc[data['pubmed_id'] == pmid, ent.label_].empty:
-                data.loc[data['pubmed_id'] == pmid, ent.label_] = ent.text
-            else:
-                data.loc[data['pubmed_id'] == pmid, ent.label_] +=  ent.text + '|' 
-
-data.to_csv("output/NER_summary.csv", sep=',')
+extract_ner_from_data('input/raw_data.csv', 'output/NER_summary.csv', 'Model/scibert_scivocab_cased', id_col='pmid', abstract_col='article_text', separator=',')
